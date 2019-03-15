5ce83a006838503393d4986597d03d2973c41785
David Feinzimer <kernelpop@users.noreply.github.com>
2019-03-15 05:04:29 -0700

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,25 +1,25 @@
 CREATE TABLE IF NOT EXISTS  users
-            (user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-             email TEXT NOT NULL UNIQUE,
-             display_name TEXT NOT NULL,
-             pass_hash TEXT NOT NULL);
+  (user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+   email TEXT NOT NULL UNIQUE,
+   display_name TEXT NOT NULL,
+   pass_hash TEXT NOT NULL);
 
 CREATE TABLE IF NOT EXISTS articles
-            (article_id INTEGER PRIMARY KEY AUTOINCREMENT,
-             title text NOT NULL,
-             content text NOT NULL,
-             headline text,
-             author text NOT NULL,
-             article_date text NOT NULL,
-             last_modified text NOT NULL);
+  (article_id INTEGER PRIMARY KEY AUTOINCREMENT,
+   title text NOT NULL,
+   content text NOT NULL,
+   headline text,
+   author text NOT NULL,
+   article_date text NOT NULL,
+   last_modified text NOT NULL);
 
 CREATE TABLE IF NOT EXISTS comments
-            (comment_id INTEGER PRIMARY KEY AUTOINCREMENT,
-             user_id text NOT NULL,
-             comment text NOT NULL,
-             comment_date text NOT NULL);
+  (comment_id INTEGER PRIMARY KEY AUTOINCREMENT,
+   user_id text NOT NULL,
+   comment text NOT NULL,
+   comment_date text NOT NULL);
 
 CREATE TABLE IF NOT EXISTS tags
-                         (tag_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                          tag text NOT NULL,
-                           url text);
+  (tag_id INTEGER PRIMARY KEY AUTOINCREMENT,
+   tag text NOT NULL,
+   url text);
