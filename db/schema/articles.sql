54a9728116b1f9407fd99001210213abcf34cbd4
David Feinzimer <d@fein.io>
2019-04-07 16:02:15 -0700

    04.07.2019.16.02

--- db/schema/articles.sql
+++ db/schema/articles.sql
@@ -1,9 +1,3 @@
-CREATE TABLE IF NOT EXISTS  users
-  (user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-   email TEXT NOT NULL UNIQUE,
-   display_name TEXT NOT NULL,
-   pass_hash TEXT NOT NULL);
-
 CREATE TABLE IF NOT EXISTS articles
   (article_id INTEGER PRIMARY KEY AUTOINCREMENT,
    title text NOT NULL,
@@ -12,14 +6,3 @@ CREATE TABLE IF NOT EXISTS articles
    author text NOT NULL,
    article_date text NOT NULL,
    last_modified text NOT NULL);
-
-CREATE TABLE IF NOT EXISTS comments
-  (comment_id INTEGER PRIMARY KEY AUTOINCREMENT,
-   user_id text NOT NULL,
-   comment text NOT NULL,
-   comment_date text NOT NULL);
-
-CREATE TABLE IF NOT EXISTS tags
-  (tag_id INTEGER PRIMARY KEY AUTOINCREMENT,
-   tag text NOT NULL,
-   url text);
---
+++ db/schema/tags.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS tags
+  (tag_id INTEGER PRIMARY KEY AUTOINCREMENT,
+   tag text NOT NULL,
+   url text);
