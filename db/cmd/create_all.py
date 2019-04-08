c65594b553d1885769af1e9b46423c4341b166ee
David Feinzimer <d@fein.io>
2019-04-07 18:40:55 -0700

    04.07.2019.18.40

--- db/cmd/create_all.py
+++ db/cmd/create_all.py
@@ -9,13 +9,13 @@ import sqlite3
 conn = sqlite3.connect('../db/articles.db')
 print("Opened articles.db successfully");
 conn.execute('CREATE TABLE IF NOT EXISTS `articles`\
-  (`article_id` INTEGER PRIMARY KEY AUTOINCREMENT,\
-   `title` text NOT NULL,\
-   `content` text NOT NULL,\
-   `headline` text,\
-   `author` text NOT NULL,\
-   `article_date` text NOT NULL,\
-   `last_modified` text NOT NULL);'
+  (`article_id`    INTEGER PRIMARY KEY AUTOINCREMENT,\
+   `title`         TEXT NOT NULL,\
+   `content`       TEXT NOT NULL,\
+   `headline`      TEXT,\
+   `author`        TEXT NOT NULL,\
+   `article_date`  TEXT NOT NULL,\
+   `last_modified` TEXT NOT NULL);'
 )
 conn.close()
 print("articles table created successfully",'\n');
@@ -25,10 +25,10 @@ print("articles table created successfully",'\n');
 conn = sqlite3.connect('../db/comments.db')
 print("Opened comments.db successfully");
 conn.execute('CREATE TABLE IF NOT EXISTS `comments`\
-  (`comment_id` INTEGER PRIMARY KEY AUTOINCREMENT,\
-   `user_id text` NOT NULL,\
-   `comment text` NOT NULL,\
-   `comment_date` text NOT NULL);'
+  (`comment_id`   INTEGER PRIMARY KEY AUTOINCREMENT,\
+   `user_id`      TEXT NOT NULL,\
+   `comment`      TEXT NOT NULL,\
+   `comment_date` TEXT NOT NULL);'
 )
 conn.close()
 print("comments table created successfully",'\n');
@@ -39,8 +39,8 @@ conn = sqlite3.connect('../db/tags.db')
 print("Opened tags.db successfully");
 conn.execute('CREATE TABLE IF NOT EXISTS tags\
   (`tag_id` INTEGER PRIMARY KEY AUTOINCREMENT,\
-   `tag` text NOT NULL,\
-   `url` text);')
+   `tag`    TEXT NOT NULL,\
+   `url`    TEXT);')
 conn.close()
 print("tags table created successfully",'\n');
 
@@ -49,10 +49,10 @@ print("tags table created successfully",'\n');
 conn = sqlite3.connect('../db/users.db')
 print("Opened users.db successfully");
 conn.execute('CREATE TABLE `users`\
-    ( `user_id`	INTEGER NOT NULL UNIQUE, \
-    `email`	TEXT NOT NULL UNIQUE, \
-    `pass_hash`	INTEGER NOT NULL,\
-    `display_name`	TEXT NOT NULL UNIQUE,\
+    ( `user_id`	      INTEGER NOT NULL UNIQUE, \
+      `email`	      TEXT NOT NULL UNIQUE, \
+      `pass_hash`     INTEGER NOT NULL,\
+      `display_name`  TEXT NOT NULL UNIQUE,\
     PRIMARY KEY(`user_id`));')
 conn.close()
 print("users table created successfully",'\n');
--- db/cmd/fill_all.py
+++ db/cmd/fill_all.py
@@ -1,17 +1,56 @@
 # https://www.tutorialspoint.com/flask/flask_sqlite.htm
 
-# This script is not completed nor working yet as of 6:13 PM April 7, 2019
+
 
 import sqlite3
+
+
+
+conn = sqlite3.connect('../db/articles.db')
+if(conn != None):
+  print("Opened articles.db successfully");
+  conn.execute("INSERT INTO articles VALUES(null,'The best article ever','This is the best article ever written','A great article!','David Feinzimer','4/7/19','4/7/19')")
+  conn.execute("INSERT INTO articles VALUES(null,'My great article'     ,'There once was a lightbulb'           ,'Yup, it is true' ,'David Feinzimer','4/2/19','4/6/19')")
+  conn.commit()
+  conn.close()
+  print("articles table filled successfully",'\n');
+else:
+  print("Cannot open articles.db")
+
+
+
+conn = sqlite3.connect('../db/comments.db')
+if(conn != None):
+  print("Opened comments.db successfully");
+  conn.execute("INSERT INTO comments VALUES(null,1,'What a great article','4/7/19')")
+  conn.commit()
+  conn.close()
+  print("comments table filled successfully",'\n');
+else:
+  print("Cannot open comments.db")
 
 
 
 conn = sqlite3.connect('../db/tags.db')
 if(conn != None):
   print("Opened tags.db successfully");
-  print(conn.execute("INSERT INTO test VALUES('apples','oranges')"))
-  print(conn.commit())
-  print('Closing:',conn.close())
+  conn.execute("INSERT INTO tags VALUES(null,'apples','test.com')")
+  conn.execute("INSERT INTO tags VALUES(null,'bananas','test.com')")
+  conn.execute("INSERT INTO tags VALUES(null,'corn','test.com')")
+  conn.commit()
+  conn.close()
   print("tags table filled successfully",'\n');
 else:
   print("Cannot open tags.db")
+
+
+
+conn = sqlite3.connect('../db/users.db')
+if(conn != None):
+  print("Opened users.db successfully");
+  conn.execute("INSERT INTO users VALUES(null,'test@email.com',54,'dfeinzimer')")
+  conn.commit()
+  conn.close()
+  print("users table filled successfully",'\n');
+else:
+  print("Cannot open users.db")
