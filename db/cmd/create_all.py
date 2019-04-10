ae906b8bf8375dfe3b6cd8f54bbd08f32c2d11e0
yroush <32183395+yroush@users.noreply.github.com>
2019-04-09 17:15:32 -0700

    Add files via upload

--- db/cmd/create_all.py
+++ db/cmd/create_all.py
@@ -9,13 +9,14 @@ import sqlite3
 conn = sqlite3.connect('../db/articles.db')
 print("Opened articles.db successfully");
 conn.execute('CREATE TABLE IF NOT EXISTS `articles`\
-  (`article_id`    INTEGER PRIMARY KEY AUTOINCREMENT,\
-   `title`         TEXT NOT NULL,\
-   `content`       TEXT NOT NULL,\
-   `headline`      TEXT,\
-   `author`        TEXT NOT NULL,\
-   `article_date`  TEXT NOT NULL,\
-   `last_modified` TEXT NOT NULL);'
+  (`article_id`           INTEGER PRIMARY KEY AUTOINCREMENT,\
+   `title`                TEXT NOT NULL,\
+   `content`              TEXT NOT NULL,\
+   `headline`             TEXT,\
+   `author`               TEXT NOT NULL,\
+   `article_date`         TEXT NOT NULL,\
+   `user_display_name`    TEXT NOT NULL,\
+   `last_modified`        TEXT NOT NULL);'
 )
 conn.close()
 print("articles table created successfully",'\n');
@@ -26,7 +27,7 @@ conn = sqlite3.connect('../db/comments.db')
 print("Opened comments.db successfully");
 conn.execute('CREATE TABLE IF NOT EXISTS `comments`\
   (`comment_id`   INTEGER PRIMARY KEY AUTOINCREMENT,\
-   `user_id`      TEXT NOT NULL,\
+   `user_display_name`      TEXT NOT NULL,\
    `comment`      TEXT NOT NULL,\
    `comment_date` TEXT NOT NULL);'
 )
@@ -51,7 +52,7 @@ print("Opened users.db successfully");
 conn.execute('CREATE TABLE `users`\
     ( `user_id`	      INTEGER NOT NULL UNIQUE, \
       `email`	      TEXT NOT NULL UNIQUE, \
-      `pass_hash`     INTEGER NOT NULL,\
+      `pass_hash`     TEXT NOT NULL,\
       `display_name`  TEXT NOT NULL UNIQUE,\
     PRIMARY KEY(`user_id`));')
 conn.close()
--- db/cmd/fill_all.py
+++ db/cmd/fill_all.py
@@ -9,8 +9,8 @@ import sqlite3
 conn = sqlite3.connect('../db/articles.db')
 if(conn != None):
   print("Opened articles.db successfully");
-  conn.execute("INSERT INTO articles VALUES(null,'The best article ever','This is the best article ever written','A great article!','David Feinzimer','4/7/19','4/7/19')")
-  conn.execute("INSERT INTO articles VALUES(null,'My great article'     ,'There once was a lightbulb'           ,'Yup, it is true' ,'David Feinzimer','4/2/19','4/6/19')")
+  conn.execute("INSERT INTO articles VALUES(null,'The best article ever','This is the best article ever written','A great article!','David Feinzimer','4/7/19', 'dfeinzimer', '4/7/19')")
+  conn.execute("INSERT INTO articles VALUES(null,'My great article'     ,'There once was a lightbulb'           ,'Yup, it is true' ,'David Feinzimer','4/2/19', 'dfeinzimer', '4/6/19')")
   conn.commit()
   conn.close()
   print("articles table filled successfully",'\n');
@@ -22,7 +22,7 @@ else:
 conn = sqlite3.connect('../db/comments.db')
 if(conn != None):
   print("Opened comments.db successfully");
-  conn.execute("INSERT INTO comments VALUES(null,1,'What a great article','4/7/19')")
+  conn.execute("INSERT INTO comments VALUES(null,'dfeinzimer','What a great article','4/7/19')")
   conn.commit()
   conn.close()
   print("comments table filled successfully",'\n');
@@ -48,7 +48,7 @@ else:
 conn = sqlite3.connect('../db/users.db')
 if(conn != None):
   print("Opened users.db successfully");
-  conn.execute("INSERT INTO users VALUES(null,'test@email.com',54,'dfeinzimer')")
+  conn.execute("INSERT INTO users VALUES(null,'test@email.com','93942e96f5acd83e2e047ad8fe03114d','dfeinzimer')")
   conn.commit()
   conn.close()
   print("users table filled successfully",'\n');
