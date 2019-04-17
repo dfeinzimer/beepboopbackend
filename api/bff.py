4ae35057776677284713197edd59b28a3373f7e9
David Feinzimer <d@fein.io>
2019-04-16 23:49:49 -0700

    04.16.2019.23.49

--- api/bff.py
+++ api/bff.py
@@ -52,7 +52,53 @@ def feed_summary():
 # A full feed containing the full text for each article, its tags as RSS categories, and a comment count.
 @app.route('/rss/full', methods=['GET'])
 def feed_full():
-    pass
+    # Make an empty list to hold rfeed items
+    feed_items = []
+
+    article_request = requests.get('http://localhost/articles/recent/10', auth=('dfeinzimer@gmail.com','password1'))
+    article_loaded = json.loads(article_request.text)
+
+    for x in article_loaded:
+        print("Article:",x['article_id'])
+        print("\t"+x['content'])
+        content = x['content']
+
+        tag_request = requests.get('http://localhost/tags/url/articles='+str(x['article_id']), auth=('dfeinzimer@gmail.com','password1'))
+        tag_loaded = json.loads(tag_request.text)
+
+        categories = []
+        if tag_loaded:
+            for y in tag_loaded:
+                print("\t\t#"+y['tag'])
+                categories.append(y['tag'])
+
+        comment_request = requests.get('http://localhost/comments/count/articles='+str(x['article_id']), auth=('dfeinzimer@gmail.com','password1'))
+        comment_loaded = json.loads(comment_request.text)
+        for z in comment_loaded:
+            print("\t\t\tComment Count:",z['count'])
+            count = z['count']
+
+        print("Content:",content)
+        print("Categories:",categories)
+        print("Count:",count)
+
+        item = Item(
+            description = content,
+            categories = categories,
+            comments = count
+        )
+        feed_items.append(item)
+
+    feed = Feed(
+        title = "Full Article Feed",
+        link = "http://localhost/rss/full",
+        description = "A feed containing the full text for each article, its tags as RSS categories, and a comment count.",
+        language = "en-US",
+        lastBuildDate = datetime.datetime.now(),
+        items = feed_items
+    )
+
+    return (feed.rss())
 
 # A comment feed for each article.
 @app.route('/rss/comments', methods=['GET'])
--- api/bff_dev.py
+++ api/bff_dev.py
@@ -8,19 +8,50 @@ import os
 
 # A feed containing the full text for each article, its tags as RSS categories, and a comment count.
 
-# Get the article data
-article_request = requests.get('http://localhost/articles/recent/10', auth=('dfeinzimer@gmail.com','password1'))
+# Make an empty list to hold rfeed items
+feed_items = []
 
-# Parse the article data
+article_request = requests.get('http://localhost/articles/recent/10', auth=('dfeinzimer@gmail.com','password1'))
 article_loaded = json.loads(article_request.text)
 
 for x in article_loaded:
     print("Article:",x['article_id'])
-    print("\t",x['content'])
+    print("\t"+x['content'])
+    content = x['content']
+
     tag_request = requests.get('http://localhost/tags/url/articles='+str(x['article_id']), auth=('dfeinzimer@gmail.com','password1'))
     tag_loaded = json.loads(tag_request.text)
+
+    categories = []
     if tag_loaded:
         for y in tag_loaded:
-            print("\t\t",y['tag'])
-# Make an empty list to hold rfeed items
-feed_items = []
+            print("\t\t#"+y['tag'])
+            categories.append(y['tag'])
+
+    comment_request = requests.get('http://localhost/comments/count/articles='+str(x['article_id']), auth=('dfeinzimer@gmail.com','password1'))
+    comment_loaded = json.loads(comment_request.text)
+    for z in comment_loaded:
+        print("\t\t\tComment Count:",z['count'])
+        count = z['count']
+
+    print("Content:",content)
+    print("Categories:",categories)
+    print("Count:",count)
+
+    item = Item(
+        description = content,
+        categories = categories,
+        comments = count
+    )
+    feed_items.append(item)
+
+feed = Feed(
+    title = "Full Article Feed",
+    link = "http://localhost/rss/full",
+    description = "A feed containing the full text for each article, its tags as RSS categories, and a comment count.",
+    language = "en-US",
+    lastBuildDate = datetime.datetime.now(),
+    items = feed_items
+)
+
+return (feed.rss())
