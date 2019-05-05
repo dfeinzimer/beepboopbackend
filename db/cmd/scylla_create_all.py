c312754ded0d67369772910324141c27d008b988
David Feinzimer <d@fein.io>
2019-05-05 14:37:35 -0700

    05.05.2019.14.37

--- db/cmd/scylla_create_all.py
+++ db/cmd/scylla_create_all.py
@@ -26,44 +26,42 @@ session.execute(
 
 
 
-'''
-conn = sqlite3.connect('../db/comments.db')
-print("Opened comments.db successfully");
-conn.execute('CREATE TABLE IF NOT EXISTS `comments`\
-  (`comment_id`   INTEGER PRIMARY KEY AUTOINCREMENT,\
-   `user_display_name`      TEXT NOT NULL,\
-   `comment`      TEXT NOT NULL,\
-   `article_url` TEXT NOT NULL,\
-   `comment_date` TEXT NOT NULL);'
+'''#############################################################################
+Create an comments table
+#############################################################################'''
+session.execute(
+    """CREATE TABLE IF NOT EXISTS comments(
+        comment_id int PRIMARY KEY,
+        user_display_name text,
+        comment text,
+        article_url text,
+        comment_date text
+    )"""
 )
-conn.execute('CREATE INDEX commentURL_idx ON comments(article_url);')
-conn.close()
-print("comments table created successfully",'\n');
 
 
 
-conn = sqlite3.connect('../db/tags.db')
-print("Opened tags.db successfully");
-conn.execute('CREATE TABLE IF NOT EXISTS tags\
-  (`tag_id` INTEGER PRIMARY KEY AUTOINCREMENT,\
-   `tag`    TEXT NOT NULL,\
-   `url`    TEXT);')
-conn.execute('CREATE INDEX tag_idx ON tags(tag);')
-conn.execute('CREATE INDEX url_idx ON tags(url);')
-conn.close()
-print("tags table created successfully",'\n');
+'''#############################################################################
+Create a tags table
+#############################################################################'''
+session.execute(
+    """CREATE TABLE IF NOT EXISTS tags(
+        tag_id int PRIMARY KEY,
+        tag text,
+        url text
+    )"""
+)
 
 
 
-conn = sqlite3.connect('../db/users.db')
-print("Opened users.db successfully");
-conn.execute('CREATE TABLE `users`\
-    ( `user_id`	      INTEGER NOT NULL UNIQUE, \
-      `email`	      TEXT NOT NULL UNIQUE, \
-      `pass_hash`     TEXT NOT NULL,\
-      `display_name`  TEXT NOT NULL UNIQUE,\
-    PRIMARY KEY(`user_id`));')
-conn.execute('CREATE INDEX email_idx ON users(email);')
-conn.close()
-print("users table created successfully",'\n');
-'''
+'''#############################################################################
+Create a users table
+#############################################################################'''
+session.execute(
+    """CREATE TABLE IF NOT EXISTS users(
+        user_id int PRIMARY KEY,
+        email text,
+        pass_hash text,
+        display_name text
+    )"""
+)
