80b9e6dcef1268b5b46361ccb476582f7c935c7e
David Feinzimer <d@fein.io>
2019-05-06 16:06:33 -0700

    05.06.2019.16.06

--- db/cmd/scylla_fill_all.py
+++ db/cmd/scylla_fill_all.py
@@ -23,9 +23,72 @@ session.execute(
         last_modified
     ) VALUES (
         uuid(),
-        'The best article title ever',
-        'Great article content 2',
-        'A good headline 2',
+        'Article title 1',
+        'Article content 1',
+        'Article headline 1',
+        'David Feinzimer',
+        '5/6/2019',
+        'dfeinzimer',
+        '5/6/2019'
+    );"""
+)
+session.execute(
+    """INSERT INTO articles (
+        article_id,
+        title,
+        content,
+        headline,
+        author,
+        article_date,
+        user_display_name,
+        last_modified
+    ) VALUES (
+        uuid(),
+        'Article title 2',
+        'Article content 2',
+        'Article headline 2',
+        'David Feinzimer',
+        '5/6/2019',
+        'dfeinzimer',
+        '5/6/2019'
+    );"""
+)
+session.execute(
+    """INSERT INTO articles (
+        article_id,
+        title,
+        content,
+        headline,
+        author,
+        article_date,
+        user_display_name,
+        last_modified
+    ) VALUES (
+        uuid(),
+        'Article title 3',
+        'Article content 3',
+        'Article headline 3',
+        'David Feinzimer',
+        '5/6/2019',
+        'dfeinzimer',
+        '5/6/2019'
+    );"""
+)
+session.execute(
+    """INSERT INTO articles (
+        article_id,
+        title,
+        content,
+        headline,
+        author,
+        article_date,
+        user_display_name,
+        last_modified
+    ) VALUES (
+        uuid(),
+        'Article title 4',
+        'Article content 4',
+        'Article headline 4',
         'David Feinzimer',
         '5/6/2019',
         'dfeinzimer',
@@ -48,7 +111,52 @@ session.execute(
     ) VALUES (
         uuid(),
         'articles/1',
-        'What a great article',
+        'Article comment 1',
+        '5/6/2019',
+        'dfeinzimer'
+    );"""
+)
+session.execute(
+    """INSERT INTO comments (
+        comment_id,
+        article_url,
+        comment,
+        comment_date,
+        user_display_name
+    ) VALUES (
+        uuid(),
+        'articles/2',
+        'Article comment 2',
+        '5/6/2019',
+        'dfeinzimer'
+    );"""
+)
+session.execute(
+    """INSERT INTO comments (
+        comment_id,
+        article_url,
+        comment,
+        comment_date,
+        user_display_name
+    ) VALUES (
+        uuid(),
+        'articles/2',
+        'Article comment 3',
+        '5/6/2019',
+        'dfeinzimer'
+    );"""
+)
+session.execute(
+    """INSERT INTO comments (
+        comment_id,
+        article_url,
+        comment,
+        comment_date,
+        user_display_name
+    ) VALUES (
+        uuid(),
+        'articles/1',
+        'Article comment 4',
         '5/6/2019',
         'dfeinzimer'
     );"""
@@ -70,6 +178,28 @@ session.execute(
         'articles/1'
     );"""
 )
+session.execute(
+    """INSERT INTO tags (
+        tag_id,
+        tag,
+        url
+    ) VALUES (
+        uuid(),
+        'myTag2',
+        'articles/2'
+    );"""
+)
+session.execute(
+    """INSERT INTO tags (
+        tag_id,
+        tag,
+        url
+    ) VALUES (
+        uuid(),
+        'myTag3',
+        'articles/1'
+    );"""
+)
 
 
 
@@ -83,7 +213,40 @@ session.execute(
         email
     ) VALUES (
         uuid(),
-        'user3',
+        'user1',
         'user1@gmail.com'
+    );"""
+)
+session.execute(
+    """INSERT INTO users (
+        user_id,
+        display_name,
+        email
+    ) VALUES (
+        uuid(),
+        'user2',
+        'user2@gmail.com'
+    );"""
+)
+session.execute(
+    """INSERT INTO users (
+        user_id,
+        display_name,
+        email
+    ) VALUES (
+        uuid(),
+        'user3',
+        'user3@gmail.com'
+    );"""
+)
+session.execute(
+    """INSERT INTO users (
+        user_id,
+        display_name,
+        email
+    ) VALUES (
+        uuid(),
+        'user4',
+        'user4@gmail.com'
     );"""
 )
