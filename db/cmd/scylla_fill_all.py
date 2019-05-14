422e3ddc9abf3235ea1e4e7ce6ca595dc7ea014d
David Feinzimer <d@fein.io>
2019-05-13 22:06:46 -0700

    05.13.2019.22.06

--- db/cmd/scylla_fill_all.py
+++ db/cmd/scylla_fill_all.py
@@ -1,3 +1,6 @@
+import hashlib
+import uuid
+
 '''#############################################################################
 The number of entries to add in each table.
 #############################################################################'''
@@ -5,6 +8,8 @@ num_articles = 10
 num_comments = 10
 num_tags = 10
 num_users = 10
+
+new_user_ids = []
 
 
 '''#############################################################################
@@ -14,6 +19,36 @@ from cassandra.cluster import Cluster
 cluster = Cluster(['172.17.0.2'])
 session = cluster.connect()
 session.set_keyspace('beepboopbackend')
+
+
+'''#############################################################################
+Fill the users table
+#############################################################################'''
+for x in range(0,num_users):
+    newid = uuid.uuid1()
+    new_user_ids.append(newid)
+    password = "password"+str(x)
+    pass_hash = hashlib.md5(password.encode())
+    session.execute(
+        """
+        INSERT INTO users (
+            user_id,
+            email,
+            display_name,
+            pass_hash
+        )
+        VALUES (%s, %s, %s, %s)
+        """,
+        (
+            newid,
+            "email"+str(x)+"@email.com",
+            "user"+str(x),
+            str(pass_hash.hexdigest())
+        )
+    )
+print("Created new users:")
+for x in new_user_ids:
+    print("\t",x)
 
 
 '''#############################################################################
@@ -77,22 +112,5 @@ for x in range(0,num_tags):
             uuid(),
             'myTag"""+str(x)+"""',
             'articles/1'
-        );"""
-    )
-
-
-'''#############################################################################
-Fill the users table
-#############################################################################'''
-for x in range(0,num_users):
-    session.execute(
-        """INSERT INTO users (
-            user_id,
-            display_name,
-            email
-        ) VALUES (
-            uuid(),
-            'user"""+str(x)+"""',
-            'user"""+str(x)+"""@gmail.com'
         );"""
     )
